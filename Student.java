8f11d4d6165c1d685840153492d4d76df4bc09d7
saikrishnarao1000 <saikrishna.rao1000@gmail.com>
2024-01-11 10:08:15 -0600

    Student program for name

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+
+    String getName() {
+        return name;
+    }
+     public static void main(String[] args){
+        Student student = new Student();
+        student.name= "Saikrishna";
+        System.out.print(student.getName());
+
+     }
+    }
